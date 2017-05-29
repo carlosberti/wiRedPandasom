9ebdb45f02a39e49f27acaafcf8d943d19103923
carlosberti <carlosberti1@hotmail.com>
2017-05-29 20:33:52 -0300

    Update label_image.py

--- label_image.py
+++ label_image.py
@@ -25,5 +25,5 @@ with tf.Session() as sess:
 
     top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
     
-    
-    print(label_lines[0])
+    for i in {0..3}:
+    	print(label_lines[0])
